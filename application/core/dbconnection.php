4d0d2ed2d46a1852f1deb5ed71471c7aa612c8aa
Garikk <supergarikk@gmail.com>
2016-10-20 19:53:29 +0300

    reinit old work

--- application/core/dbconnection.php
+++ application/core/dbconnection.php
@@ -15,7 +15,7 @@ class dbconnection {
     private $dbconn;
     
     function __construct() {
-         $this->dbconn = mysqli_connect("127.0.0.1","u0133909_default","kkcar123","kkcar");
+         $this->dbconn = mysqli_connect("127.0.0.1","kkcar","kkcar","kkcar");
     }
     
     public function ExecQuery($query)//,$params)
