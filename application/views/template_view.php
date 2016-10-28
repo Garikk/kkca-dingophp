f4b323151e849b834ac4960ab4218a61631b681f
Garikk <supergarikk@gmail.com>
2016-10-28 10:15:16 +0300

    add analytics

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -32,7 +32,7 @@
   </head>
 
   <body>
-
+<?php include_once("analyticstracking.php") ?>
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
