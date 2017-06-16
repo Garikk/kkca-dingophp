79e931007f63fffdb4bf20a32d6eb0397daa96ff
Garikk <supergarikk@gmail.com>
2017-06-16 19:04:44 +0300

    last commit to old-style

---
+++ application/models/model_managectrlr.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of model_kkcontroller
+ *
+ * @author blinov_is
+ */
+class model_managectrlr extends Model {
+
+    function __construct() {
+        $this->dbc = new dbconnection();
+    }
+
+    public function get_plugins() {
+
+        return $this->dbc->ExecQuery(
+                       " SELECT "
+                       ."         plugins.id, "
+                        ."        plugins.uuid, "
+                        ."        plugins.name, "
+                        ."        plugins.description, "
+                        ."        plugins.pins_in, "
+                        ."        plugins.pins_out "
+                        ." FROM "
+                        ."        plugins");
+    
+
+    }
+}
+    
